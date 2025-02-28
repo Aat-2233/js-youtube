773179a66661f6980f2f12046e42a2002e5c6bde
Aat-2233 <142422104+Aat-2233@users.noreply.github.com>
2025-02-28 17:45:11 +0000

    01-basics

---
+++ 01-basics/variables.js
@@ -0,0 +1,10 @@
+const account_id = "12345"
+let account_email = "aatreyee@google.com"
+var account_password ="345"
+account_city= "kolkata"
+console.log(account_id);
+account_email="shifa@hchc.com"
+account_city ="delhi"
+account_password="452"
+ let account_state;
+console.table([account_email , account_password , account_id , account_city, account_state])
