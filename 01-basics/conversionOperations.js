9e64d1b88ab3220e2f5b04a406c62acee1072887
Aat-2233 <142422104+Aat-2233@users.noreply.github.com>
2025-03-09 14:03:08 +0000

    Conversion Operations

---
+++ 01-basics/conversionOperations.js
@@ -0,0 +1,21 @@
+let score = "33abc"
+console.log(typeof score)
+
+let valueinNum= Number(score)
+console.log(typeof valueinNum)
+console.log(valueinNum)
+
+//"33"-> 33 
+//"33abc"->Nan
+//"null"->0
+//true ->1 , false->0
+let islogged = 1
+let inBoolean = Boolean(islogged)
+console.log(inBoolean)
+// 1->true , 0->false
+//"aatreyee"->true
+//" "->false 
+let number = 45 
+let stringNumber = String(number)
+console.log(typeof(stringNumber))
+console.log(stringNumber);
