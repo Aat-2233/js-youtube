f0227057d5d24ec05fb185cf4424e5d3b9921bd9
Aat-2233 <142422104+Aat-2233@users.noreply.github.com>
2025-03-09 13:29:13 +0000

    datatypes

---
+++ 01-basics/datatypes.js
@@ -0,0 +1,9 @@
+"use strict"; //treats all js code as newer version
+let name = "Aatreyee" //string 
+
+let age = 18 //integer , bigint
+let state // undefined 
+
+console.log(typeof "Aatreyee")
+console.log(typeof age)
+
