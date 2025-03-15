315f4b4695b808ae9b050607fe8dd4d9b659a4fd
Aat-2233 <142422104+Aat-2233@users.noreply.github.com>
2025-03-15 07:57:54 +0000

    comparison

---
+++ 01-basics/comparison.js
@@ -0,0 +1,7 @@
+console.log("2">1) //datatype is converted 
+console.log(null>0)
+console.log(null==0);
+console.log(null>=0);
+//equality check and comparison are both diferent in javascript 
+//=== - checks if both are same data type 
+console.log("2"===2);
