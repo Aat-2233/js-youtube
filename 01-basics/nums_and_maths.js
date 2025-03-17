274d9cd2d09424e98fe5b73c0fd760ac6457607f
Aat-2233 <142422104+Aat-2233@users.noreply.github.com>
2025-03-17 13:09:13 +0000

    numbers

---
+++ 01-basics/nums_and_maths.js
@@ -0,0 +1,26 @@
+const score = 400 ;
+console.log(score);
+const value = new Number(100);
+console.log(value);
+console.log(value.toString().length);
+console.log(value.toFixed(2));
+const two = 23.68594;
+console.log(two.toPrecision(3));
+const hundreds=2000000000; //puts commas 
+console.log(hundreds.toLocaleString("en-IN"));
+
+//+++++++MATHs++++++++++++++++++++++++++++++++++++++++++
+console.log(Math.abs(-4));
+console.log(Math.round(4.69));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(5.8));
+console.log(Math.min(5,8,6,9,4,7,5));
+console.log(Math.max(100,1000,500));
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+const max = 200;
+const min = 100 ;
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+
+
