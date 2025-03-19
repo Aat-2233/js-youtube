0e1d2a62f54fd449ab00684cdd0479b78cd0b292
Aat-2233 <142422104+Aat-2233@users.noreply.github.com>
2025-03-19 09:56:05 +0000

    array

---
+++ 02_basics'/array.js
@@ -0,0 +1,17 @@
+const myarr=[0,1,2,3,4]
+const arr1=["Aatreyee","soumili","ayesha","sampriti"]
+const arr2 = new Array(4,5,6,7)
+//console.log(myarr[0]);
+console.log(arr1[1]);
+console.log(arr2[2]);
+
+//Array methods
+myarr.push(6);
+myarr.push(8);
+myarr.pop();
+myarr.unshift(9) //adds value to the first
+myarr.shift();
+myarr.shift(); //removes value from the first 
+
+console.log(myarr);
+
