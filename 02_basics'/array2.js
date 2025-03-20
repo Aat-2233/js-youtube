612fae8f72996ac136ebced55279f6ada85b00cb
Aat-2233 <142422104+Aat-2233@users.noreply.github.com>
2025-03-20 18:54:15 +0000

    array2

---
+++ 02_basics'/array2.js
@@ -0,0 +1,22 @@
+const flowers =[ "lily", "rose", "sunflower" , "marigold"];
+const fruits =["watermelon", "guava", "apple","orange"];
+ const arr = flowers.concat(fruits); //adds arrays 
+console.log(arr);
+//another way of adding arrays(more usable)
+const newarr=[...flowers , ...fruits];
+console.log(newarr);
+
+
+const mixarr= [1,2,3,[4,5,6,],7,8[9,10,[11,12]]];
+const arr1=mixarr.flat(3);
+console.log(arr1);
+
+console.log(Array.isArray("aatreyee"));
+console.log(Array.from("aatreyee")); //converts to array
+
+let score1 =100;
+let score2=200;
+let score3 = 300;
+console.log(Array.of(score1,score2,score3));
+
+
