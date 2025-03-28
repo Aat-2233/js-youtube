89f2d44e841c4a013f1d16f1353f836941a17492
Aat-2233 <142422104+Aat-2233@users.noreply.github.com>
2025-03-28 11:38:12 +0000

    func

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -18,5 +18,45 @@ function userlogin(username){
 console.log(userlogin("Aatreyee"));
 console.log(userlogin());
 
+//(...) this is the rest operator 
+function calculatecartprice(...num){
+    return num;
+
+}
+console.log(calculatecartprice(200,300,400)); //creates an array and stores the values.
+
+//another function
+function calculatecartprice1(val1,val2,...num){
+    return num;
+}
+console.log(calculatecartprice1(200,300,400,500));//we see that the first two value is assigned to val and val2 and the rest values are displayed.
+
+
+//passing an object into the function 
+const user={
+    username:"Aatreyee",
+    price:1999
+};
+
+function handleobject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+};
+handleobject(user);
+//another way of implementing the function 
+handleobject({
+    username:"sam",
+    price:200
+})
+
+//passing array into the function
+const myarr=[100,200,300,400]
+function returnsecondvalue(getarray){
+    return getarray[1];
+}
+console.log(returnsecondvalue(myarr));
+
+
+
 
 
