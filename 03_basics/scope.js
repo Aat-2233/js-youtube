bea130c3235fbae868590bfc73593a0dcf738213
Aat-2233 <142422104+Aat-2233@users.noreply.github.com>
2025-04-02 08:00:46 +0000

    scopes

--- 03_basics/scope.js
+++ 03_basics/scope.js
@@ -7,3 +7,33 @@ let c = 30;
 console.log(b);
 //console.log(c);
 
+//nested scopes 
+function one(){
+    const username="Aatreyee"
+
+    function two(){
+        const website = "youtube";
+        console.log(username); //username can take its data because for function two it is a global variable
+        
+    }
+    //console.log(website); gives error because website is beyond the scope;
+    two();
+}
+one();
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++
+function addone(num){
+    return num+1;
+}
+addone(5)
+console.log(addone);
+
+//another way of declaring function
+ const addtwo= function(num){
+    return num+2;
+}
+console.log(addtwo(5));
+
+
+
+
